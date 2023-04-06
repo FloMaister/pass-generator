f8ac31689380d1d58cbaa203ff012ffe0f4e7c92
Artyom <artyom.tishevich@yandex.ru>
2023-04-06 20:09:31 +0300

    page layout mb rdy

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,45 @@
-import { Component } from "react";
+import { useState } from "react";
 
 import './app.scss';
 
 const App = (props) => {
     
     return (
-        <h1>Hello world!</h1>
+        <div className="wrapper">
+            <div className="item">
+                <input type="text" 
+                className="item__box" 
+                value="Select options"/>
+                <form className="item__form">
+                    <label 
+                    className="item__label" 
+                    htmlFor="number">
+                        Password length
+                    </label>
+                    <div className="item__wrapper-range">
+                        <input 
+                        className="item__input-number" 
+                        type="text" name="number" 
+                        id="number"
+                        value="3" />
+
+                        <input className="item__range" type="range" name="range" id="range" />
+                    </div>
+
+                    <div className="item__wrapper-numbers">
+                        <label htmlFor="checkbox-number">Numbers</label>
+                        <input type="checkbox" name="numbers" id="checkbox-numbers" />
+                    </div>
+                    
+                    <div className="item__wrapper-symbols">
+                        <label htmlFor="checkbox-symbols">Symbols</label>   
+                        <input type="checkbox" name="symbols" id="checkbox-symbols" />
+                    </div>
+                    
+                    <input className="item__submit" type="submit" value="Generate"/>
+                </form>
+            </div>
+        </div>
     )
 }
 
